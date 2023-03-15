b6cc9fe0d27e90b3bf72ae5e71392ad8835e0337
oliver <yplpf96@gmail.com>
2023-03-16 06:03:17 +0800

    调整线程安全集合

--- ThreadSafeList.cs
+++ ThreadSafeList.cs
@@ -1,22 +1,153 @@
-﻿using System;
-using System.Collections.Concurrent;
-using System.Collections.Generic;
-
+﻿
 namespace Pingfan.Kit
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Threading;
+
     /// <summary>
-    /// 线程安全的List, ConcurrentBag的包装
+    /// 封装的线程安全集合
     /// </summary>
-    public class ThreadSafeList<T> : ConcurrentBag<T>
+    /// <typeparam name="T"></typeparam>
+    public class ThreadSafeList<T>
     {
-        public ThreadSafeList()
+        private readonly List<T> _list = new List<T>();
+        private readonly ReaderWriterLockSlim _lock = new ReaderWriterLockSlim();
+
+        public void Add(T item)
         {
+            _lock.EnterWriteLock();
+            try
+            {
+                _list.Add(item);
+            }
+            finally
+            {
+                _lock.ExitWriteLock();
+            }
         }
 
-        public ThreadSafeList(IEnumerable<T> collection) : base(collection)
+        public bool Remove(T item)
         {
+            _lock.EnterWriteLock();
+            try
+            {
+                return _list.Remove(item);
+            }
+            finally
+            {
+                _lock.ExitWriteLock();
+            }
         }
 
-        // 其他方法可以根据需要添加
+        public bool Contains(T item)
+        {
+            _lock.EnterReadLock();
+            try
+            {
+                return _list.Contains(item);
+            }
+            finally
+            {
+                _lock.ExitReadLock();
+            }
+        }
+
+        public int IndexOf(T item)
+        {
+            _lock.EnterReadLock();
+            try
+            {
+                return _list.IndexOf(item);
+            }
+            finally
+            {
+                _lock.ExitReadLock();
+            }
+        }
+
+        public void Insert(int index, T item)
+        {
+            _lock.EnterWriteLock();
+            try
+            {
+                _list.Insert(index, item);
+            }
+            finally
+            {
+                _lock.ExitWriteLock();
+            }
+        }
+
+        public void RemoveAt(int index)
+        {
+            _lock.EnterWriteLock();
+            try
+            {
+                _list.RemoveAt(index);
+            }
+            finally
+            {
+                _lock.ExitWriteLock();
+            }
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                _lock.EnterReadLock();
+                try
+                {
+                    return _list[index];
+                }
+                finally
+                {
+                    _lock.ExitReadLock();
+                }
+            }
+            set
+            {
+                _lock.EnterWriteLock();
+                try
+                {
+                    _list[index] = value;
+                }
+                finally
+                {
+                    _lock.ExitWriteLock();
+                }
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                _lock.EnterReadLock();
+                try
+                {
+                    return _list.Count;
+                }
+                finally
+                {
+                    _lock.ExitReadLock();
+                }
+            }
+        }
+
+        public void Clear()
+        {
+            _lock.EnterWriteLock();
+            try
+            {
+                _list.Clear();
+            }
+            finally
+            {
+                _lock.ExitWriteLock();
+            }
+        }
     }
+
 }
