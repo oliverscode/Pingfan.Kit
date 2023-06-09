71ff12580ee8c7a11d0d4a4dd15161d2c47952bb
oliver <yplpf96@gmail.com>
2023-06-10 00:10:38 +0800

    更新判断是否重复运行

--- ProcessEx.cs
+++ ProcessEx.cs
@@ -1,42 +1,33 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using System.Threading;
 
 namespace Pingfan.Kit
 {
     public static class ProcessEx
     {
-        private static string MutexName = Assembly.GetExecutingAssembly().GetType().GUID.ToString();
+        private static string MutexName = $"{Assembly.GetEntryAssembly().FullName}_{Environment.UserInteractive}";
 
-        /// <summary>
-        /// 判断是否运行过
-        /// </summary>
         public static bool HasRun
         {
             get
             {
-                try
+                // 尝试打开Mutex
+                bool createdNew;
+                using (var mutex = new Mutex(true, MutexName, out createdNew))
                 {
-                    // 尝试获取 Mutex
-                    Mutex.OpenExisting(MutexName);
-                    // 如果没有抛出异常，说明 Mutex 已经存在，即程序已经运行过
-                    return true;
+                    if (createdNew)
+                    {
+                        // Mutex不存在，程序之前没有运行过
+                        mutex.ReleaseMutex();
+                        return false;
+                    }
                 }
-                catch (WaitHandleCannotBeOpenedException)
-                {
-                    // WaitHandleCannotBeOpenedException 说明 Mutex 不存在，即程序没有运行过
-                    // 创建 Mutex
-                    new Mutex(false, MutexName);
-                    return false;
-                }
-                catch (AbandonedMutexException)
-                {
-                    // AbandonedMutexException 说明 Mutex 被遗弃，即上一个程序实例没有正确释放 Mutex
-                    // 但是操作系统已经帮我们接管并释放了这个 Mutex，所以我们可以安全地忽略这个异常
-                    // 并创建一个新的 Mutex
-                    new Mutex(false, MutexName);
-                    return false;
-                }
+
+                // Mutex已存在，程序已经运行过
+                return true;
             }
         }
     }
+
 }
