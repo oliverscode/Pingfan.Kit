c28f5151785b4e12f0f4b90f993fc19b2c4cd2a1
oliver <yplpf96@gmail.com>
2023-07-15 20:20:51 +0800

    增加依赖注入

---
+++ App.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace Pingfan.Kit
+{
+#if net48 || NETCOREAPP
+    using LightInject;
+
+    public class App
+    {
+        private static IServiceContainer _container = new ServiceContainer();
+
+        public static void ConfigServices(Action<IServiceContainer> action)
+        {
+            action(_container);
+        }
+    }
+#endif
+}
