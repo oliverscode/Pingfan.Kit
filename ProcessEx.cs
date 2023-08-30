3415d4e3e36dd75598bcbcb4adacc97941e4b812
Oliver <yplpf96@gmail.com>
2023-08-31 02:11:34 +0800

    增加进程判断

--- ProcessEx.cs
+++ ProcessEx.cs
@@ -1,14 +1,22 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Threading;
 
 namespace Pingfan.Kit
 {
+    /// <summary>
+    /// 进程扩展
+    /// </summary>
     public static class ProcessEx
     {
         private static string _mutexName = $"{Assembly.GetEntryAssembly().FullName}_{Environment.UserInteractive}";
         private static Mutex _mutex;
 
+        /// <summary>
+        /// 本程序是否运行过
+        /// </summary>
         public static bool HasRun
         {
             get
@@ -26,5 +34,46 @@ namespace Pingfan.Kit
                 return true;
             }
         }
+
+#if net48 || NETCOREAPP
+        
+        /// <summary>
+        /// 是否是管理员运行, 如果是linux则判断是否是root用户
+        /// </summary>
+        public static bool IsAdmin
+        {
+            get
+            {
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    try
+                    {
+                        Process process = new Process();
+                        ProcessStartInfo startInfo = new ProcessStartInfo
+                        {
+                            WindowStyle = ProcessWindowStyle.Hidden,
+                            FileName = "cmd.exe",
+                            Arguments = "/C net session",
+                            RedirectStandardOutput = true,
+                            UseShellExecute = false,
+                            CreateNoWindow = true
+                        };
+                        process.StartInfo = startInfo;
+                        process.Start();
+                        process.WaitForExit();
+                        return process.ExitCode == 0;
+                    }
+                    catch
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    return Environment.UserName == "root";
+                }
+            }
+        }
+#endif
     }
 }
