6217a366267a8f19ed3b0dd1d767bef36f07a049
oliver <yplpf96@gmail.com>
2023-11-28 17:22:22 +0800

    再次优化JSON

--- src/JsonEx.cs
+++ src/JsonEx.cs
@@ -1,28 +1,36 @@
-﻿
-#if NETSTANDARD3_1 || NET6
+﻿#if NETSTANDARD3_1 || NET6
 
 namespace Pingfan.Kit
 {
     using System.Text.Unicode;
     using System.Text.Json;
-    
+
     /// <summary>
     /// JSON序列化
     /// </summary>
-    public class Json
+    public static class Json
     {
-        internal static readonly JsonSerializerOptions _JsonSerializerOptions = new JsonSerializerOptions()
+        /// <summary>
+        /// JSON序列化配置
+        /// </summary>
+        public static readonly JsonSerializerOptions JsonSerializerOptions = new JsonSerializerOptions()
         {
             // 中文支持
             Encoder = System.Text.Encodings.Web.JavaScriptEncoder.Create(UnicodeRanges.All),
 
+#if NET6
             // 忽略空值
+            DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull,
+#else
+            // 忽略默认值
             IgnoreNullValues = true,
+#endif
+
 
             // 全部大写参照上面注释代码
             // PropertyNamingPolicy = new UpperCaseNamingPolicy(),
         };
-        
+
         /// <summary>
         /// 把对象转换为JSON字符串
         /// </summary>
@@ -30,44 +38,79 @@ namespace Pingfan.Kit
         /// <returns></returns>
         public static string ToString(object obj)
         {
-            return JsonSerializer.Serialize(obj, _JsonSerializerOptions);
+            return JsonSerializer.Serialize(obj, JsonSerializerOptions);
         }
-        
+
         /// <summary>
         /// 一段JSON字符串转换为对象
         /// </summary>
-        /// <param name="json"></param>
-        /// <typeparam name="T"></typeparam>
-        /// <returns></returns>
-        public static T FromString<T>(string json)
+        public static T FromString<T>(string json, T defaultValue = default!)
         {
-            return JsonSerializer.Deserialize<T>(json, _JsonSerializerOptions);
+            try
+            {
+                return JsonSerializer.Deserialize<T>(json, JsonSerializerOptions)!;
+            }
+            catch
+            {
+                return defaultValue;
+            }
         }
     }
 
+    /// <summary>
+    /// JSON扩展
+    /// </summary>
     public static class JsonEx
     {
         /// <summary>
         /// 把对象转换为JSON字符串
         /// </summary>
-        /// <param name="obj"></param>
-        /// <returns></returns>
         public static string ToJsonString(this object obj)
         {
             return Json.ToString(obj);
         }
-        
+
         /// <summary>
         /// 把JSON字符串转换为对象
         /// </summary>
-        /// <param name="json"></param>
-        /// <typeparam name="T"></typeparam>
-        /// <returns></returns>
-        public static T ToJson<T>(this string json)
+        public static T FromJsonString<T>(this string json, T defaultValue = default!)
         {
-            return Json.FromString<T>(json);
+            return Json.FromString<T>(json, defaultValue);
         }
     }
-    
+
+    /// <summary>
+    /// JsonElement扩展
+    /// </summary>
+    public static class JsonElementEx
+    {
+#if NET6
+        /// <summary>
+        /// 获取JsonElement的值
+        /// </summary>
+        /// <param name="jObject"></param>
+        /// <param name="key"></param>
+        /// <param name="defaultValue"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static T Get<T>(this JsonElement jObject, string key, T defaultValue = default(T))
+        {
+            try
+            {
+                if (jObject.TryGetProperty(key, out var value))
+                {
+                    return value.Deserialize<T>()!;
+                }
+            }
+            catch
+            {
+                // ignored
+            }
+
+            return defaultValue;
+        }
+
+#endif
+    }
 }
 #endif
