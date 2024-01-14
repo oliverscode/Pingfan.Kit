42be55e4b3e169623e75a755b2822cbf503aa3da
Oliver <yplpf96@gmail.com>
2024-01-15 04:21:33 +0800

    修改缓存可以为null

--- src/WebServer/HttpResponseDefault.cs
+++ src/WebServer/HttpResponseDefault.cs
@@ -3,7 +3,6 @@ using System.IO;
 using System.Net;
 using System.Text;
 using System.Text.Json;
-using System.Text.Json.Serialization;
 using Pingfan.Kit.WebServer.Interfaces;
 
 namespace Pingfan.Kit.WebServer;
