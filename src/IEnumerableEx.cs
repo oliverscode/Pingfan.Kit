191a1ef1d106696e51fffb46792c067ebe46ede7
Oliver <yplpf96@gmail.com>
2024-03-23 01:14:42 +0800

    多线程遍历增加进度

--- src/IEnumerableEx.cs
+++ src/IEnumerableEx.cs
@@ -54,6 +54,55 @@ namespace Pingfan.Kit
         }
 
         /// <summary>
+        /// 并行处理集合中的每一个元素。
+        /// </summary>
+        public static async Task Each<T>(this IEnumerable<T> list, Action<T, Progress> callBack, int threadCount = 0)
+        {
+            var queue = new ConcurrentQueue<T>(list);
+            threadCount = threadCount <= 0 ? Environment.ProcessorCount * 2 : threadCount;
+            threadCount = threadCount > queue.Count ? queue.Count : threadCount;
+
+            var pe = new Progress();
+            pe.Total = list.Count();
+            var tasks = Enumerable.Range(0, threadCount)
+                .Select(_ => Task.Run(() =>
+                {
+                    while (queue.TryDequeue(out var data))
+                    {
+                        callBack(data, pe);
+                        pe.Next();
+                    }
+                }));
+
+            await Task.WhenAll(tasks);
+        }
+
+        /// <summary>
+        /// 并行处理集合中的每一个元素。
+        /// </summary>
+        public static async Task EachAsync<T>(this IEnumerable<T> list, Func<T, Progress, Task> callBack,
+            int threadCount = 0)
+        {
+            var queue = new ConcurrentQueue<T>(list);
+            threadCount = threadCount <= 0 ? Environment.ProcessorCount * 2 : threadCount;
+            threadCount = threadCount > queue.Count ? queue.Count : threadCount;
+
+            var pe = new Progress();
+            pe.Total = list.Count();
+            var tasks = Enumerable.Range(0, threadCount)
+                .Select(_ => Task.Run(async () =>
+                {
+                    while (queue.TryDequeue(out var data))
+                    {
+                        await callBack(data, pe);
+                        pe.Next();
+                    }
+                }));
+
+            await Task.WhenAll(tasks);
+        }
+
+        /// <summary>
         /// 从集合中随机返回一个元素。
         /// </summary>
         public static T RandomOne<T>(this IEnumerable<T> collection)
@@ -78,7 +127,7 @@ namespace Pingfan.Kit
         {
             return list.Any(p => p.Equals(value, StringComparison.OrdinalIgnoreCase));
         }
-        
+
         /// <summary>
         /// 克隆一个集合
         /// </summary>
