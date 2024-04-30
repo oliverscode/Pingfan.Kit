1b2b9ae621c0a4d474524d653f09ca237307da66
Oliver <yplpf96@gmail.com>
2024-04-30 14:22:23 +0800

    发布版本

--- src/WebServer/Middlewares/MidWebSocket.cs
+++ src/WebServer/Middlewares/MidWebSocket.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Net.WebSockets;
 using System.Text;
 using System.Threading;
@@ -28,6 +29,11 @@ public class MidWebSocket : IMiddleware
     /// 默认UTF-8编码    
     /// </summary>
     public Encoding Encoding { get; set; } = Encoding.UTF8;
+
+    // /// <summary>
+    // /// 最大消息长度
+    // /// </summary>
+    // public int MaxMessageSize { get; set; } = 1024 * 4;
 
     /// <inheritdoc />
     public void Invoke(IContainer container, IHttpContext ctx, Action next)
@@ -61,27 +67,54 @@ public class MidWebSocket : IMiddleware
 
 
         // 接收数据
+        var ms = new MemoryStream();
         var buffer = new byte[1024 * 4];
+        // while (webSocketContext.IsAvailable)
+        // {
+        //     var result = webSocketContext.WebSocket.ReceiveAsync(new ArraySegment<byte>(buffer),
+        //         CancellationToken.None).Result;
+        //     if (result.MessageType == WebSocketMessageType.Close)
+        //     {
+        //         break;
+        //     }
+        //
+        //     if (result.MessageType == WebSocketMessageType.Binary)
+        //     {
+        //         handler.OnBinary(webSocketContext, buffer);
+        //     }
+        //     else if (result.MessageType == WebSocketMessageType.Text)
+        //     {
+        //         var txt = Encoding.GetString(buffer, 0, result.Count);
+        //         handler.OnMessage(webSocketContext, txt);
+        //     }
+        // }
         while (webSocketContext.IsAvailable)
         {
-            var result = webSocketContext.WebSocket.ReceiveAsync(new ArraySegment<byte>(buffer),
-                CancellationToken.None).Result;
+            var result =  webSocketContext.WebSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None).Result;
             if (result.MessageType == WebSocketMessageType.Close)
             {
                 break;
             }
 
-            if (result.MessageType == WebSocketMessageType.Binary)
+            ms.Write(buffer, 0, result.Count);
+
+            if (result.EndOfMessage)
             {
-                handler.OnBinary(webSocketContext, buffer);
-            }
-            else if (result.MessageType == WebSocketMessageType.Text)
-            {
-                var txt = Encoding.GetString(buffer, 0, result.Count);
-                handler.OnMessage(webSocketContext, txt);
+                ms.Seek(0, SeekOrigin.Begin);
+                if (result.MessageType == WebSocketMessageType.Binary)
+                {
+                    handler.OnBinary(webSocketContext, ms.ToArray());
+                }
+                else if (result.MessageType == WebSocketMessageType.Text)
+                {
+                    var txt = Encoding.GetString(ms.ToArray());
+                    handler.OnMessage(webSocketContext, txt);
+                }
+                ms.SetLength(0); // 重置MemoryStream以便重新使用
             }
         }
-
+        
+        
         handler.OnClosed(webSocketContext);
     }
 
